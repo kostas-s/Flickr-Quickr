bed9a7b3583a40e5f5df6bed299bc847aa518d9c
kostas-s <ksmonos@hotmail.com>
2021-02-13 20:49:08 +0200

    some finishing touches before deploy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,36 @@
 class StaticPagesController < ApplicationController
+  before_action :check_safe_tags
 
+  # Some whitelisted words for safe searching
+  @@whitelisted_words = ['dogs', 'cats', 'landscape', 'trains', 'travel',
+                         'guitars']
+
+  # Initialize API connection settings
   require 'flickraw'
   FlickRaw.api_key = Figaro.env.flickraw_api_key
   FlickRaw.shared_secret = Figaro.env.flickraw_shared_secret
 
   def index
+    @whitelisted_words = @@whitelisted_words
+
     if params[:user] && params[:user] != '' && params[:tag] && params[:tag] != ''
       @images = flickr.photos.search(username: params[:user], tags: params[:tag], safe_search: 1)
     elsif params[:user] && params[:user] != ''
       @images = flickr.photos.search(username: params[:user], safe_search: 1)
     elsif params[:tag] && params[:tag] != ''
       @images = flickr.photos.search(tags: params[:tag], safe_search: 1)
+    end
+  end
+
+  private
+
+  def check_safe_tags
+    if params[:tag]
+      params[:tag].downcase.split(' ') do |tag|
+        unless tag.chomp.in? @@whitelisted_words
+          redirect_to '/'
+        end
+      end
     end
   end
 
